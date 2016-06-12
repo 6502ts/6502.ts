7a68bc1d5cd368f33a0da3d56096ef1ee47cb579
Christian Speckner <christian.speckner@mayflower.de>
2016-06-12 16:43:25 +0200

    Change latched input to match spec -> golf works. Who would have thought that the spec meant THAT?

--- src/machine/stella/tia/LatchedInput.ts
+++ src/machine/stella/tia/LatchedInput.ts
@@ -4,12 +4,6 @@ export default class LatchedInput {
 
     constructor(private _switch: SwitchInterface) {
         this.reset();
-
-        this._switch.stateChanged.addHandler((state: boolean) => {
-            if (this._modeLatched && !state) {
-                this._latchedValue = 0x80;
-            }
-        });
     }
 
     reset(): void {
@@ -19,21 +13,22 @@ export default class LatchedInput {
 
     vblank(value: number): void {
         if ((value & 0x40) > 0) {
-            if (!this._modeLatched) {
-                this._modeLatched = true;
-                this._latchedValue = 0;
-            }
+            this._modeLatched = true;
         } else {
             this._modeLatched = false;
+            this._latchedValue = 0x80;
         }
     }
 
     inpt(): number {
+        let value = this._switch.read() ? 0 : 0x80;
+
         if (this._modeLatched) {
-            return this._latchedValue;
-        } else {
-            return this._switch.read() ? 0 : 0x80;
+            this._latchedValue &= value;
+            value = this._latchedValue;
         }
+
+        return value;
     }
 
     private _modeLatched = false;
