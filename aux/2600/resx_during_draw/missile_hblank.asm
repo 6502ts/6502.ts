762deca43cc48deccfd2ccf05dcfe0bf42a668d9
Christian Speckner <christian.speckner@mayflower.de>
2017-01-30 00:15:32 +0100

    Rework tests, add NTSC mode.

--- aux/2600/resx_during_draw/missile_hblank.asm
+++ aux/2600/resx_during_draw/missile_hblank.asm
@@ -1,55 +1,26 @@
-    PROCESSOR 6502
+BURN_LINES_PAL = 100
+SEPARATION = 10
 
-OVERSCAN_LINES = 36
-COLOR_P0 = $48
-COLOR_P1 = $0C
-DELAY = 2
-SEPARATION = 5
-BURN_LINES = 14
+    INCLUDE main.inc
 
-    INCLUDE vcs.h
-    INCLUDE macro.h
+    SEG.U VARS_TEST
+    ORG $80
 
-    SEG.U VARS
-    ORG $A0
-TestTarget DS.W
+Counter DS.B 1
 
-    SEG CODE
-    ORG $F000
-
-    MAC IndirectJSR
-.base
-    LDA #(.base + 8) >> 8
-    PHA
-    LDA #(.base + 8) & $FF
-    PHA
-    JMP ({1})
-    ENDM
-
-Delay SUBROUTINE
-
-    PHA
-    TXA
-
-    LDX #DELAY
-.wait
-    DEX
-    BNE .wait
-
-    TAX
-    PLA
-
-Noop SUBROUTINE
-    RTS
+    SEG CODE_TEST
+    ORG $F200
 
 Test1 SUBROUTINE
 
     CLC
+    LDA #12
+    STA Counter
     LDA #$A0
     LDX #0
     LDY #02
 
-    REPEAT 12
+.loop
 
     STA HMM0
     STA HMM1
@@ -92,7 +63,8 @@ Test1 SUBROUTINE
     ADC #$10
     EOR #$80
 
-    REPEND
+    DEC Counter
+    BNE .loop
 
     LDX #SEPARATION
 .skipline
@@ -105,11 +77,13 @@ Test1 SUBROUTINE
 Test2 SUBROUTINE
 
     CLC
+    LDA #12
+    STA Counter
     LDA #$A0
     LDX #0
     LDY #02
 
-    REPEAT 12
+.loop
 
     STA HMM0
     STA HMM1
@@ -152,7 +126,8 @@ Test2 SUBROUTINE
     ADC #$10
     EOR #$80
 
-    REPEND
+    DEC Counter
+    BNE .loop
 
     LDX #SEPARATION
 .skipline
@@ -161,117 +136,6 @@ Test2 SUBROUTINE
     BNE .skipline
 
     RTS
-
-Start SUBROUTINE
-    SEI
-    CLD
-    LDX #$FF
-    TXS
-    LDA #0
-ClearMem
-    STA 0,X
-    DEX
-    BNE ClearMem
-
-    LDA #COLOR_P0
-    STA COLUP0
-
-    LDA #COLOR_P1
-    STA COLUP1
-
-MainLoop
-
-Vsync
-    ; line 1
-    LDA #2
-    STA VSYNC
-    STA VBLANK
-    STA WSYNC
-
-    ; line 2
-    STA WSYNC
-
-    ; line 3
-    STA WSYNC
-
-Vblank
-    LDA #56
-    STA TIM64T
-    LDA #0
-    STA VSYNC
-    STA ENAM0
-    STA ENAM1
-
-    LDA SWCHB
-    AND #$08
-    BNE PrepareTest2
-
-PrepareTest1
-    LDA #Test1 & $FF
-    STA TestTarget
-    LDA #Test1 >> 8
-    STA TestTarget + 1
-    JMP AfterPrepareTest
-
-PrepareTest2
-    LDA #Test2 & $FF
-    STA TestTarget
-    LDA #Test2 >> 8
-    STA TestTarget + 1
-
-AfterPrepareTest
-
-BurnVblank
-    LDA INTIM
-    BNE BurnVblank
-
-    LDA #0
-    STA VBLANK
-
-    STA WSYNC
-
-Kernal
-
-    LDA #$30
-    STA NUSIZ0
-    STA NUSIZ1
-
-    IndirectJSR TestTarget
-
-    LDA #$20
-    STA NUSIZ0
-    STA NUSIZ1
-
-    IndirectJSR TestTarget
-
-    LDA #$10
-    STA NUSIZ0
-    STA NUSIZ1
-
-    IndirectJSR TestTarget
-
-    LDA #0
-    STA NUSIZ0
-    STA NUSIZ1
-
-    IndirectJSR TestTarget
-
-    LDX #BURN_LINES
-BurnLines
-    STA WSYNC
-    DEX
-    BNE BurnLines
-
-    LDA #2
-    STA VBLANK
-    LDX #OVERSCAN_LINES
-
-BurnOverscan
-    STA WSYNC
-    DEX
-    BNE BurnOverscan
-
-    JMP  MainLoop
 
     ORG $FFFC
     .WORD Start
