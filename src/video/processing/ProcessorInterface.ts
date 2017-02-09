24d065ed55a602ce2a609c3f534eae2e587b09fc
Christian Speckner <christian.speckner@mayflower.de>
2017-02-09 23:12:37 +0100

    Refactoring, pave the road for image processing

---
+++ src/video/processing/ProcessorInterface.ts
@@ -0,0 +1,16 @@
+import {EventInterface} from 'microevent.ts';
+
+import RGBASurfaceInterface from '../surface/RGBASurfaceInterface';
+import PoolMemberInterface from '../../tools/pool/PoolMemberInterface';
+
+interface ProcessorInterface {
+
+    init(width: number, height: number): void;
+
+    flush(): void;
+
+    processSurface(surface: PoolMemberInterface<RGBASurfaceInterface>): void;
+
+    emit: EventInterface<PoolMemberInterface<RGBASurfaceInterface>>;
+
+}
