76de72eb315c1c3c03c307e94f31cf1e223e6b58
Christian Speckner <christian.speckner@mayflower.de>
2018-01-05 01:48:10 +0100

    Add license preample to minified files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,31 @@ const path = require('path'),
 
 const TSLINT_OPTIONS = `--project ./tsconfig.json --config ./tslint.json --exclude '**/*.js'`;
 
+const LICENSE_PREAMPLE = `
+/*
+ *   This file is part of 6502.ts, an emulator for 6502 based systems built
+ *   in Typescript.
+ *
+ *   Copyright (C) 2014 - 2018 Christian Speckner & contributors
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   Please go to the project's github page at
+ *
+ *   https://github.com/6502ts/6502.ts
+ *
+ *   for more details and for a copy of the GNU General Public License v2.
+ */
+`;
+
 module.exports = function(grunt) {
     'use strict';
 
@@ -203,7 +228,8 @@ module.exports = function(grunt) {
 
         uglify: {
             options: {
-                sourceMap: true
+                sourceMap: true,
+                banner: LICENSE_PREAMPLE
             },
             stellerator: {
                 dest: 'build/stellerator/js/app.js',
