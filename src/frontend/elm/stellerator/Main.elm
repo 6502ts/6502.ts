94adfbbc7d10165ea4ea39f8e1ed50d9649631f9
Christian Speckner <christian.speckner@mayflower.de>
2019-05-25 00:17:56 +0200

    More style games.

--- src/frontend/elm/stellerator/Main.elm
+++ src/frontend/elm/stellerator/Main.elm
@@ -1,8 +1,10 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
+import Css exposing (..)
+import Html
+import Html.Styled exposing (..)
+import Html.Styled.Attributes exposing (..)
 
 
 type Model
@@ -25,15 +27,46 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor i
 """
 
 
+panel : String -> List (Attribute msg) -> List (Html msg) -> Html msg
+panel label attr content =
+    div attr [ div [ class "panel" ] (content ++ [ div [ class "panel-label" ] [ text label ] ]) ]
+
+
 view : Model -> Html Msg
 view model =
-    div [ class "panel" ]
-        [ div [ class "panel-label" ]
-            [ text "some label:" ]
-        , p
-            []
-            [ text loremIpsum ]
-        , p [] [ text "hello world!" ]
+    div
+        [ class "panel"
+        , css
+            [ Css.height <| calc (vh 100) minus (rem 1.2)
+            , boxSizing borderBox
+            , displayFlex
+            , flexDirection row
+            , flexWrap Css.wrap
+            , Css.property "align-content" "flex-start"
+            ]
+        ]
+        [ panel "panel 1"
+            [ css [ flex <| pct 50 ] ]
+            [ div [ class "panel-label" ]
+                [ text "Panel 1:" ]
+            , p
+                []
+                [ text loremIpsum ]
+            , p [] [ text "hello world!" ]
+            ]
+        , panel "panel 2"
+            [ css [ flex <| pct 50 ] ]
+            [ div [ class "panel-label" ]
+                [ text "Panel 2:" ]
+            , p
+                []
+                [ text loremIpsum ]
+            , p [] [ text "hello world!" ]
+            ]
+        , panel "panel 3"
+            [ css [ flexGrow <| num 1 ] ]
+            [ text "Toolbar"
+            ]
         ]
 
 
@@ -41,5 +74,5 @@ main =
     Browser.sandbox
         { init = Empty
         , update = update
-        , view = view
+        , view = view >> toUnstyled
         }
